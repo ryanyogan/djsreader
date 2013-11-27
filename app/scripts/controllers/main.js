5586de1353190c5237efb5c930746d44c99a516d
Ryan Yogan <ryanyogan@gmail.com>
2013-11-27 18:18:44 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('djsreaderApp')
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
+    $scope.refreshInterval = 60;
+
+    $scope.feeds = [{
+      url: 'http://dailyjs.com/atom.xml',
+      items: [ /* Blog post goes here */ ]
+    }];
+
+    $scope.addFeed = function(feed) {
+      $scope.feeds.push(feed);
+      $scope.fetchFeed(feed);
+      $scope.newFeed = {};
+    };
+
+    $scope.deleteFeed = function(feed) {
+      $scope.feeds.splice($scope.feeds.indexOf(feed), 1);
+    };
+
+    $scope.fetchFeed = function(feed) {
+      feed.items = [];
+
+      var apiUrl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'";
+      apiUrl += encodeURIComponent(feed.url);
+      apiUrl += "'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+      $http.jsonp(apiUrl).
+        success(function(data, status, headers, config) {
+          if (data.query.results) {
+            feed.items = data.query.results.entry;
+          }
+        }).
+        error(function(data, status, headers, config) {
+          console.error('Error fetching feed:', data);
+        });
+      $timeout(function() { $scope.fetchFeed(feed) }, $scope.refreshInterval * 1000);
+    };
+
+  $scope.fetchFeed($scope.feeds[0]);
+});
